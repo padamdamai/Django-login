64d4b6b3f0514ce9671c30808217662a1d230827
Padam Damai <padamdamai888@gmail.com>
2024-03-28 18:01:28 +0545

    connected database and set models

--- projectApp/models.py
+++ projectApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class LogIn(models.Model):
+    userName = models.CharField(max_length=200)
+    password = models.IntegerField(max_length=300)
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.userName
+        
